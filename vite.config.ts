00986c3d16eac33aa1970fd66d8b941f996d5e34
quochuydev <quochuy.dev@gmail.com>
2025-08-04 11:24:33 +0700

    feat: github page build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/woocommerce-talk-ai/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  }
 })
